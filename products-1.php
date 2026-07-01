681ace44054e2b7d140aef3e3729ad118137df40
supportdcbindia <support@dcbindia.in>
2026-07-01 11:13:44 +0530

    changes

---
+++ products-1.php
@@ -0,0 +1,145 @@
+<?php include("header.php");?>
+<div class="rs-breadcrumbs">
+    <div class="breadcrumbs-image">
+        <img src="images/inner-pages/breadcums/products.webp" alt="breadcum">
+        <div class="breadcrumbs-inner">
+            <div class="container">
+                <div class="breadcrumbs-text">
+                    <h1 class="breadcrumbs-title">Our Products</h1>
+                    <ul class="breadcrumbs-subtitle">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="javascript:;">Our Products</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="product-landing space">
+    <div class="container">
+        <div class="row gy-4">
+            <div class="col-xl-4 col-lg-6 col-sm-12" data-animation="fadeInUp" data-delay="0.1" data-duration="1.2">
+                <div class="single-service-hr">
+                    <a href="online-ups-1-in-1-out.php" class="thumbnail">
+                        <img src="images/inner-pages/products/online-ups/online-ups-1in-1-out/online-ups-1-in-1-out.webp" alt="Online Ups 1 In 1 Out">
+                    </a>
+                    <div class="inner">
+                        <a href="online-ups-1-in-1-out.php">
+                            <h3 class="title">Online Ups 1 In 1 Out</h3>
+                        </a>
+                       <a href="online-ups-1-in-1-out.php" class="theme-btn btn-style-one">
+                            <span class="btn-title">Read more</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-6 col-sm-12" data-animation="fadeInUp" data-delay="0.1" data-duration="1.2">
+                <div class="single-service-hr">
+                    <a href="online-ups-3-in-1-out.php" class="thumbnail">
+                        <img src="images/inner-pages/products/online-ups/online-ups-3-in-1-out/online-ups-3-in-1-out.webp" alt="Online Ups 3 In 1 Out">
+                    </a>
+                    <div class="inner">
+                        <a href="online-ups-3-in-1-out.php">
+                            <h3 class="title">Online Ups 3 In 1 Out</h3>
+                        </a>
+                       <a href="online-ups-3-in-1-out.php" class="theme-btn btn-style-one">
+                            <span class="btn-title">Read more</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-6 col-sm-12" data-animation="fadeInUp" data-delay="0.1" data-duration="1.2">
+                <div class="single-service-hr">
+                    <a href="online-ups-3-in-3-out.php" class="thumbnail">
+                        <img src="images/inner-pages/products/online-ups/online-ups-3-in-3-out/1.webp" alt="Online Ups 3 In 3 Out">
+                    </a>
+                    <div class="inner">
+                        <a href="online-ups-3-in-3-out.php">
+                            <h3 class="title">Online Ups 3 In 3 Out</h3>
+                        </a>
+                       <a href="online-ups-3-in-3-out.php" class="theme-btn btn-style-one">
+                            <span class="btn-title">Read more</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-6 col-sm-12" data-animation="fadeInUp" data-delay="0.1" data-duration="1.2">
+                <div class="single-service-hr">
+                    <a href="static-voltage-stabilizer.php" class="thumbnail">
+                        <img src="images/inner-pages/products/voltage-stabilizer/static-voltage-stabilizer/static-voltage-stabilizer.webp" alt="Static Voltage Stabilizer">
+                    </a>
+                    <div class="inner">
+                        <a href="static-voltage-stabilizer.php">
+                            <h3 class="title">Static Voltage Stabilizer</h3>
+                        </a>
+                       <a href="static-voltage-stabilizer.php" class="theme-btn btn-style-one">
+                            <span class="btn-title">Read more</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-6 col-sm-12" data-animation="fadeInUp" data-delay="0.1" data-duration="1.2">
+                <div class="single-service-hr">
+                    <a href="servo-voltage-stabilizer-1-phase.php" class="thumbnail">
+                         <img src="images/inner-pages/products/voltage-stabilizer/servo-voltage-stabilizer-phase-1/servo-voltage-stabilizer-phase-1.webp" alt="Servo Voltage Stabilizer (1-Phase)">
+                    </a>
+                    <div class="inner">
+                        <a href="servo-voltage-stabilizer-1-phase.php">
+                            <h3 class="title">Servo Voltage Stabilizer (1-Phase)</h3>
+                        </a>
+                       <a href="servo-voltage-stabilizer-1-phase.php" class="theme-btn btn-style-one">
+                            <span class="btn-title">Read more</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-6 col-sm-12" data-animation="fadeInUp" data-delay="0.1" data-duration="1.2"
+                style="transform: translate(0px, 0px); opacity: 1;">
+                <div class="single-service-hr">
+                    <a href="servo-voltage-stabilizer-3-phase.php" class="thumbnail">
+                       <img src="images/inner-pages/products/voltage-stabilizer/servo-voltage-stabilizer-phase-3/servo-voltage-stabilizer-phase-3.webp" alt="Servo Voltage Stabilizer (3-Phase)">
+                    </a>
+                    <div class="inner">
+                        <a href="servo-voltage-stabilizer-3-phase.php">
+                            <h3 class="title">Servo Voltage Stabilizer (3-Phase)</h3>
+                        </a>
+                       <a href="servo-voltage-stabilizer-3-phase.php" class="theme-btn btn-style-one">
+                            <span class="btn-title">Read more</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-6 col-sm-12" data-animation="fadeInUp" data-delay="0.1" data-duration="1.2">
+                <div class="single-service-hr">
+                    <a href="isolation-tranformer.php" class="thumbnail">
+                          <img src="images/inner-pages/products/isolation-tranformer/isolation-tranformer.webp" alt="Isolation Tranformer">
+                    </a>
+                    <div class="inner">
+                        <a href="isolation-tranformer.php">
+                            <h3 class="title">Isolation Tranformer</h3>
+                        </a>
+                       <a href="isolation-tranformer.php" class="theme-btn btn-style-one">
+                            <span class="btn-title">Read more</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-6 col-sm-12" data-animation="fadeInUp" data-delay="0.1" data-duration="1.2">
+                <div class="single-service-hr">
+                    <a href="electro-plating-rectifier.php" class="thumbnail">
+                        <img src="./images/inner-pages/products/electro-plating-rectifier/electro-plating-rectifier.webp" alt="Elevator Back-Up Ups">
+                    </a>
+                    <div class="inner">
+                        <a href="electro-plating-rectifier.php">
+                            <h3 class="title">Electro Plating Rectifier</h3>
+                        </a>
+                       <a href="electro-plating-rectifier.php" class="theme-btn btn-style-one">
+                            <span class="btn-title">Read more</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include("footer.php");?>
